4f4feb1e56a0046868bfcb4939dee1f31397bbfb
MAnojSHA5799 <manojshakya54@gmail.com>
2024-01-18 13:21:28 +0530

    frontend

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,10 +1,10 @@
 
 // import PortfolioFilter1 from ""
 import dynamic from 'next/dynamic'
-const PortfolioFilter1 = dynamic(() => import('@/components/elements/PortfolioFilter1'), {
+const PortfolioFilter1 = dynamic(() => import('../components/elements/PortfolioFilter1'), {
     ssr: false,
 })
-import Layout from "@/components/layout/Layout"
+import Layout from "../components/layout/Layout"
 import Link from "next/link"
 export default function Home() {
 
