f02b3007028ca6fcfb2c59b07b519498952afb83
MAnojSHA5799 <manojshakya54@gmail.com>
2024-02-06 13:28:37 +0530

    leave

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -32,45 +32,66 @@ export default function Home() {
         <>
             <Layout headerStyle={2} footerStyle={1} breadcrumbTitle="About Us">
                 {/* about-section */}
-                <section className="about-section p_relative pb-0 mt-5">
+                <section className="about-sections p_relative pb-0 mt-5">
                     <div className="auto-container">
                         <div className="row clearfix">
-                        <div className="col-lg-6 col-md-12 col-sm-12 image-column">
+                        <div className="col-lg-4 col-md-12 col-sm-12 image-column">
                             <div className="image_block_one">
                             <div className="image-box p_relative pr_50 mr_30">
-                            <figure className="image image-1"><img src="/assets/images/about us 550x550.jpg" alt="" /></figure>
-                            <figure className="image image-2"><img src="/assets/images/about_us_350x250.jpg" alt="" /></figure>
-                                <div className="video-inner">
+                            {/* <figure className="image image-1"><img src="/assets/images/about us 550x550.jpg" alt="" /></figure> */}
+                            {/* <figure className="image image-2"><img src="/assets/images/about_us_350x250.jpg" alt="" /></figure> */}
+                                {/* <div className="video-inner">
                                     <div className="video-btn">
                                         <VideoPopup />
                                     </div>
-                                </div>
+                                </div> */}
+                                 <video
+                    className="image image-1"
+                    loop
+                    muted
+                    autoPlay
+                    playsInline
+                    preload="false"
+                    src="/assets/animations/Animation-1.mp4/"
+                    // style={{ width: '400px', height: '40%',}}
+                style={{
+                    width: '120%', // Default width for mobile
+                    height: 'auto', // Default height for mobile
+                    '@media (min-width: 768px)': { // Adjustments for desktop screens
+                      width: '50%', // Set width for desktop
+                      height: 'auto', // Adjust height accordingly
+                    },
+                  }}
+                  />
+
                             </div>
                             </div>
                         </div>
-                        <div className="col-lg-6 col-md-12 col-sm-12 content-column">
+                        <div className="col-lg-8 col-md-12 col-sm-12 content-column">
                             <div className="content_block_one">
                             <div className="content-box p_relative ml_30">
                                 <div className="sec-title mb-1">
                                 <span className="sub-title">About  Blitz Learning Technologies</span>
-                                <h2>Dedicated And Professional Consulting Services</h2>
+                                <h2>We are the Ultimate Learning Partner for your Training Needs</h2>
                                 </div>
                                 <div className="text mb_35">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing integer ultrices suspendisse varius etiam est. Est, felis, tempus nec vitae orci sodales Metus, velit nec at diam in sed. Massa dui ipsum ornare sagittis dolor sagittis amet odio est. Sit semper et velit fusce.</p>
+                                <p>Founded in 2016, Blitz Learning Technologies was established with a goal to provide the highest quality eLearning content for an immersive experience.</p>
+                                <br />
+                                <p>The founders of Blitz Learning Technologies identified the gaps in client delivery and decided to create a mechanism of extremely customized client delivery.</p>
                                 </div>
                                 <div className="inner-box mb_35">
                                 <div className="single-item">
                                     <div className="icon-box"><i className="icon-11"></i></div>
-                                    <h3>Solution Focused</h3>
-                                    <p>Lorem ipsum dolor sit amet tempus consectetur adipiscing.</p>
+                                    <h3>Measurable Performance</h3>
+                                    <p>Putting people at the center of transformation to improve employee.</p>
                                 </div>
                                 <div className="single-item">
                                     <div className="icon-box"><i className="icon-11"></i></div>
-                                    <h3>99.99% Success</h3>
-                                    <p>Lorem ipsum dolor sit amet tempus consectetur adipiscing.</p>
+                                    <h3>Intuitive Learning</h3>
+                                    <p>Designing meaningful & engaging learning experiences that attract and engage employees across every stage in the learning journey.</p>
                                 </div>
                                 </div>
-                                <figure className="signature"><img src="assets/images/icons/signature-1.png" alt="" /></figure>
+                                {/* <figure className="signature"><img src="assets/images/icons/signature-1.png" alt="" /></figure> */}
                             </div>
                             </div>
                         </div>
