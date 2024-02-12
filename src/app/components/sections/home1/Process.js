46eea5013748f2382ed7872a9efc92ed34493e46
MAnojSHA5799 <manojshakya54@gmail.com>
2024-02-12 11:49:26 +0530

    leave

--- src/app/components/sections/home1/Process.js
+++ src/app/components/sections/home1/Process.js
@@ -1,59 +1,94 @@
-
-
 export default function Process() {
-    return (
-        <>
-            <section className="working-process-section pt-3 pb-5 centred">
-                <div className="auto-container">
-                    <div className="sec-title mb_100">
-                    <span className="sub-title">Working process</span>
-                    <h2>3 Easy steps to get quick help</h2>
-                    </div>
-                    <div className="inner-container">
-                    <div className="working-block-one">
-                        <div className="inner-box">
-                        <div className="shape" style={{ backgroundImage: "url(assets/images/shape/shape-17.png)" }}></div>
-                        <div className="count-text">
-                            <h3>01 <span>Step</span></h3>
-                        </div>
-                        <div className="text">
-                            <div className="inner">
-                            <h3>Fill in the required form</h3>
-                            <p>Amet minim mollit no duis deserunt ulamco.</p>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    <div className="working-block-one">
-                        <div className="inner-box">
-                        <div className="shape" style={{ backgroundImage: "url(assets/images/shape/shape-17.png)" }}></div>
-                        <div className="count-text">
-                            <h3>02 <span>Step</span></h3>
-                        </div>
-                        <div className="text">
-                            <div className="inner">
-                            <h3>Submit <br />Your documents</h3>
-                            <p>Amet minim mollit no duis deserunt ulamco.</p>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    <div className="working-block-one">
-                        <div className="inner-box">
-                        <div className="count-text">
-                            <h3>03 <span>Step</span></h3>
-                        </div>
-                        <div className="text">
-                            <div className="inner">
-                            <h3>Get ready to <br />Receive your visa</h3>
-                            <p>Amet minim mollit no duis deserunt ulamco.</p>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
+  return (
+    <>
+      <section className="working-process-section pt-3 pb-5 centred">
+        <div className="auto-container">
+          <div className="sec-title mb_100">
+            <span className="sub-title">Working process</span>
+            <h2>3 Easy steps to get quick help</h2>
+          </div>
+          <div className="inner-container mt-0">
+          <video
+                    className="image-services"
+                    loop
+                    muted
+                    autoPlay
+                    playsInline
+                    preload="false"
+                    // style={{ width: '300px', height: '200px' }}
+                  >
+                    <source
+                      src="/assets/animations/L&D-model_New1.mp4"
+                      type="video/mp4"
+                    />
+                  </video>
+            {/* <div className="working-block-one">
+              <div className="inner-box">
+                <div
+                  className="shape"
+                  style={{
+                    backgroundImage: "url(assets/images/shape/shape-17.png)",
+                  }}
+                ></div>
+
+                <div className="count-text">
+                  <h3>
+                    01 <span>Step</span>
+                  </h3>
                 </div>
-            </section>
-        </>
-    )
+                <div className="text">
+                  <div className="inner">
+                    <h3>Fill in the required form</h3>
+                    <p>Amet minim mollit no duis deserunt ulamco.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="working-block-one">
+              <div className="inner-box">
+                <div
+                  className="shape"
+                  style={{
+                    backgroundImage: "url(assets/images/shape/shape-17.png)",
+                  }}
+                ></div>
+                <div className="count-text">
+                  <h3>
+                    02 <span>Step</span>
+                  </h3>
+                </div>
+                <div className="text">
+                  <div className="inner">
+                    <h3>
+                      Submit <br />
+                      Your documents
+                    </h3>
+                    <p>Amet minim mollit no duis deserunt ulamco.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="working-block-one">
+              <div className="inner-box">
+                <div className="count-text">
+                  <h3>
+                    03 <span>Step</span>
+                  </h3>
+                </div>
+                <div className="text">
+                  <div className="inner">
+                    <h3>
+                      Get ready to <br />
+                      Receive your visa
+                    </h3>
+                    <p>Amet minim mollit no duis deserunt ulamco.</p>
+                  </div>
+                </div>
+              </div>
+            </div> */}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
