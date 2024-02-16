e181270402dbb61e3300716233e59897580c37a4
MAnojSHA5799 <manojshakya54@gmail.com>
2024-02-16 12:08:36 +0530

    leave

--- src/app/components/layout/Breadcrumb.js
+++ src/app/components/layout/Breadcrumb.js
@@ -1,11 +1,60 @@
+'use client'
 import Link from "next/link"
 
 export default function Breadcrumb({ breadcrumbTitle }) {
+
+
+
     return (
         <>
-            
+            {console.log(breadcrumbTitle)}
             <section className="page-title centred">
-                <div className="bg-layer" style={{ backgroundImage: 'url(/assets/header-images/Aboutus3.jpg)' }}></div>
+                <div className="bg-layer"
+                    // style={{ backgroundImage: 'url(/assets/images/Contact-Us.jpg)' }}
+
+                    // style = {{backgroundImage:breadcrumbTitle?'url(/assets/images/Contact-Us.jpg)':'none'}}
+                    style={{
+                        backgroundImage: breadcrumbTitle === "Our Services" ? 'url(/assets/header-images/ourservice.jpg)' :
+                            breadcrumbTitle === "Custom e-Learning" ? 'url(/assets/header-images/Custome_e-learning.mp4)' :
+                                breadcrumbTitle === "Content Services" ? 'url(/assets/header-images/content_services.mp4)' :
+                                    breadcrumbTitle === "Video Production" ? 'url(/assets/header-images/video_production.mp4)' :
+                                        breadcrumbTitle === "Learning Consulting" ? 'url(/assets/header-images/learning_consulting.mp4)' :
+                                            breadcrumbTitle === "Why Us" ? 'url(/assets/header-images/whyus.png)' :
+                                                breadcrumbTitle === "About Us" ? 'url(/assets/header-images/aboutus-1.jpg)' :
+                                                    breadcrumbTitle === "Get In Touch" ? 'url(/assets/header-images/contact-us.png)' :
+                                                        'none'
+                    }}
+
+                >
+                    {breadcrumbTitle === "Custom e-Learning" && (
+                        <video autoPlay loop muted className="bg-video">
+                            <source src="/assets/header-images/Custome_e-learning.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    )}
+
+                    {breadcrumbTitle === "Content Services" && (
+                        <video autoPlay loop muted className="bg-video">
+                            <source src="/assets/header-images/content_services.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    )}
+
+                    {breadcrumbTitle === "Video Production" && (
+                        <video autoPlay loop muted className="bg-video">
+                            <source src="/assets/header-images/video_production.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    )}
+
+                    {breadcrumbTitle === "Learning Consulting" && (
+                        <video autoPlay loop muted className="bg-video">
+                            <source src="/assets/header-images/learning_consulting.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    )}
+
+                </div>
                 <div className="auto-container">
                     <div className="content-box">
                         <h1>{breadcrumbTitle}</h1>
